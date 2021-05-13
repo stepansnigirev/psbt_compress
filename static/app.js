c650d2df9238635fff8109f7acece4962d8b6910
Stepan Snigirev <snigirev.stepan@gmail.com>
2021-05-13 20:14:31 +0200

    add download stuff

--- static/app.js
+++ static/app.js
@@ -7,7 +7,10 @@ async function run() {
 run();
 
 document.addEventListener("DOMContentLoaded", function(){
-    var el = document.getElementById("psbtin");
+    let el = document.getElementById("psbtin");
+    let fbtn = document.getElementById("filebtn");
+    let downloadbtn = document.getElementById("psbtout");
+
 
     if (el != null) {
         el.addEventListener("change", (e) => {
@@ -17,6 +20,7 @@ document.addEventListener("DOMContentLoaded", function(){
                 return;
             }
             let file = files[0];
+            fbtn.innerHTML = file.name;
             console.log("Loading "+file.name);
             let reader = new FileReader();
             reader.onload = function(e) {
@@ -24,11 +28,15 @@ document.addEventListener("DOMContentLoaded", function(){
                 if(buf.substring(0,4) == "psbt"){
                     buf = btoa(buf);
                 }
+                fbtn.innerHTML = `${file.name}: ${buf.length} bytes`;
                 console.log(buf.substring(0,5));
                 let compressed = compress(buf);
                 console.log(compressed.substring(0,5));
                 console.log(buf.length);
                 console.log(compressed.length);
+                downloadbtn.href="data:application/octet-stream;base64,"+compressed;
+                downloadbtn.innerHTML = `Download compressed file: ${compressed.length} bytes`;
+                downloadbtn.style.display = 'block';
             }
             // reader.readAsText(file);
             reader.readAsBinaryString(file);
